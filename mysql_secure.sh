b7e5dfe173814c7b6c7234c566ed6d5f64a61d3e
Diyar Parwana <diypa571@gmail.com>
2023-08-19 13:38:37 +0200

    Create mysql_secure.sh

---
+++ mysql_secure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Generate a strong password that will be used for mysql_secure_installation
+PASSWORD=""
+while [[ ! $(echo $PASSWORD | tr -d -c '[:lower:]' | wc -m) -ge 2 || 
+          ! $(echo $PASSWORD | tr -d -c '[:upper:]' | wc -m) -ge 2 || 
+          ! $(echo $PASSWORD | tr -d -c '[:digit:]' | wc -m) -ge 2 || 
+          ! $(echo $PASSWORD | tr -d -c '#$' | wc -m) -ge 2 ]]; do 
+  PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9#$' | fold -w 15 | head -n 1)
+done
+
+echo "Generated Password: $PASSWORD"
+# Write the password to a file
+echo "$PASSWORD" > db_password.txt
+echo "Password has been written to db_password.txt"
+
+# Running mysql_secure_installation
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn sudo mysql_secure_installation
+
+expect \"Remove test database and access to it? (Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+
+expect \"Disallow root login remotely? (Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+
+expect \"Remove test database and access to it? (Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+
+expect \"Reload privilege tables now? (Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+
+expect eof
+")
+
+echo "$SECURE_MYSQL"
