2ad69745a51baafede4842ea2ba873d93b9ccd4c
Diyar Parwana <diypa571@gmail.com>
2023-09-25 07:48:34 +0200

    Update nftables.sh

--- nftables.sh
+++ nftables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #diypa571
+
 # Update and install nftables
 sudo apt update
 sudo apt install -y nftables
@@ -10,12 +11,13 @@ sudo systemctl enable nftables
 
 # Add tables and chains for input, forward, output
 sudo nft add table inet filter
-sudo nft add chain inet filter input { type filter hook input priority filter \; }
-sudo nft add chain inet filter forward { type filter hook forward priority filter \; }
-sudo nft add chain inet filter output { type filter hook output priority filter \; }
 
-# Add rules to accept only SSH (port 22), HTTP (port 80), HTTPS (port 443) traffic
-# sudo nft add rule inet filter input tcp dport 22 accept
+# Add chains with their respective policies
+sudo nft add chain inet filter input { type filter hook input priority filter \; policy drop \; }
+sudo nft add chain inet filter forward { type filter hook forward priority filter \; policy drop \; }
+sudo nft add chain inet filter output { type filter hook output priority filter \; policy accept \; }
+
+# Add rules to accept only HTTP (port 80) and HTTPS (port 443) traffic
 sudo nft add rule inet filter input tcp dport 80 accept
 sudo nft add rule inet filter input tcp dport 443 accept
 
