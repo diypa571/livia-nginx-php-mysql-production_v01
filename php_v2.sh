dcd84e66e8b68ad713d00787985d8b48cf899334
Diyar Parwana <diypa571@gmail.com>
2024-01-13 13:38:48 +0100

    Update php_v2.sh

--- php_v2.sh
+++ php_v2.sh
@@ -36,6 +36,7 @@ for i in "${ADDR[@]}"; do
 
     # create directories
     sudo mkdir -p /var/www/$i/public_html
+    sudo mkdir -p /var/www/$i/public_html/errors
 
     # set permissions
     sudo chown -R livia:livia /var/www/$i/public_html
@@ -60,8 +61,8 @@ for i in "${ADDR[@]}"; do
         server_name $i www.$i;
 
         # Custom error pages
-        error_page 404 /404.html;
-        error_page 500 502 503 504 /500.html;
+        error_page 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 421 422 423 424 425 426 428 429 431 451 errors/404.html;
+        error_page 500 501 502 503 504 505 506 507 508 510 511 errors/500.html;
 
         location / {
             try_files \$uri \$uri/ =404;
