abc6c21cffc27704ed1f184adb946a1c0ba5c420
Diyar Parwana <diypa571@gmail.com>
2024-01-23 17:36:16 +0100

    Update php_v2.sh

--- php_v2.sh
+++ php_v2.sh
@@ -62,46 +62,48 @@ for i in "${ADDR[@]}"; do
         server_name $i www.$i;
 
         # Custom error pages
-        error_page 400 errors/400.html;
-        error_page 401 errors/401.html;
-        error_page 402 errors/402.html;
-        error_page 403 errors/403.html;
-        error_page 404 errors/404.html;
-        error_page 405 errors/405.html;
-        error_page 406 errors/406.html;
-        error_page 407 errors/407.html;
-        error_page 408 errors/408.html;
-        error_page 409 errors/409.html;
-        error_page 410 errors/410.html;
-        error_page 411 errors/411.html;
-        error_page 412 errors/412.html;
-        error_page 413 errors/413.html;
-        error_page 414 errors/414.html;
-        error_page 415 errors/415.html;
-        error_page 416 errors/416.html;
-        error_page 417 errors/417.html;
-        error_page 418 errors/418.html;
-        error_page 421 errors/421.html;
-        error_page 422 errors/422.html;
-        error_page 423 errors/423.html;
-        error_page 424 errors/424.html;
-        error_page 425 errors/425.html;
-        error_page 426 errors/426.html;
-        error_page 428 errors/428.html;
-        error_page 429 errors/429.html;
-        error_page 431 errors/431.html;
-        error_page 451 errors/451.html;
-        error_page 500 errors/500.html;
-        error_page 501 errors/501.html;
-        error_page 502 errors/502.html;
-        error_page 503 errors/503.html;
-        error_page 504 errors/504.html;
-        error_page 505 errors/505.html;
-        error_page 506 errors/506.html;
-        error_page 507 errors/507.html;
-        error_page 508 errors/508.html;
-        error_page 510 errors/510.html;
-        error_page 511 errors/511.html;
+# Custom error pages
+error_page 400 /errors/400.html;
+error_page 401 /errors/401.html;
+error_page 402 /errors/402.html;
+error_page 403 /errors/403.html;
+error_page 404 /errors/404.html;
+error_page 405 /errors/405.html;
+error_page 406 /errors/406.html;
+error_page 407 /errors/407.html;
+error_page 408 /errors/408.html;
+error_page 409 /errors/409.html;
+error_page 410 /errors/410.html;
+error_page 411 /errors/411.html;
+error_page 412 /errors/412.html;
+error_page 413 /errors/413.html;
+error_page 414 /errors/414.html;
+error_page 415 /errors/415.html;
+error_page 416 /errors/416.html;
+error_page 417 /errors/417.html;
+error_page 418 /errors/418.html;
+error_page 421 /errors/421.html;
+error_page 422 /errors/422.html;
+error_page 423 /errors/423.html;
+error_page 424 /errors/424.html;
+error_page 425 /errors/425.html;
+error_page 426 /errors/426.html;
+error_page 428 /errors/428.html;
+error_page 429 /errors/429.html;
+error_page 431 /errors/431.html;
+error_page 451 /errors/451.html;
+error_page 500 /errors/500.html;
+error_page 501 /errors/501.html;
+error_page 502 /errors/502.html;
+error_page 503 /errors/503.html;
+error_page 504 /errors/504.html;
+error_page 505 /errors/505.html;
+error_page 506 /errors/506.html;
+error_page 507 /errors/507.html;
+error_page 508 /errors/508.html;
+error_page 510 /errors/510.html;
+error_page 511 /errors/511.html;
+
      
 
         location / {
